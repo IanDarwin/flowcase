3657daef3aeb4666fe7a3fbf8307f044558e9df6
Ian Darwin <ian@darwinsys.com>
2022-12-24 11:34:50 -0500

    Revamp cards demo a bit

--- lib/card_demos.dart
+++ lib/card_demos.dart
@@ -12,16 +12,46 @@ class CardDemos extends StatefulWidget {
 class _CardDemosState extends State<CardDemos> {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Row(
+    return Column(
         children: [
-          Column(
-          children:[
-          Icon(Icons.balance_rounded),
-          Text("Savings")
-        ],)
+          Card(
+              color: Colors.greenAccent,
+              child: SizedBox(
+                  width: 250,
+                  height: 100,
+                  child: InkWell(
+                    splashColor: Colors.blue.withAlpha(30),
+                    onTap: () => debugPrint('Savings Card tapped.'),
+                    child:
+                    Row(
+                        children: const [
+                          Icon(Icons.account_balance_rounded),
+                          Text("Savings")
+                        ]
+                    ),
+
+                  )
+              )
+          ),
+          Card(
+              color: Colors.amberAccent,
+              child: SizedBox(
+                  width: 250,
+                  height: 100,
+                  child: InkWell(
+                    splashColor: Colors.blue.withAlpha(30),
+                    onTap: () => debugPrint('Checking Card tapped.'),
+                    child:
+                    Row(
+                        children: const [
+                          Icon(Icons.account_balance_rounded),
+                          Text("Checking")
+                        ]
+                    ),
+                  )
+              )
+          )
         ]
-      )
     );
   }
 }
